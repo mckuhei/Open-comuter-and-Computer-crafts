82337e40cf61d4bea193d42f444b2a58b8c8eaf9
kebufu <31839741+kebufu@users.noreply.github.com>
2017-10-21 15:29:20 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 os.remove("/init.lua")
 loadfile("/bin/cd.lua")("..")
-wget
+wget https://raw.githubusercontent.com/kebufu/Open-comuter-and-Computer-crafts/master/init.lua
 dofile("/bin/reboot.lua")
